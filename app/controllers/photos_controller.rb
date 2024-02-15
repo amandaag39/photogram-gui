235235621f611bd4a3a693783b0ab27b162c26c6
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-15 22:58:59 +0000

    started photo show

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,23 @@
 class PhotosController < ApplicationController
+
   def index
     matching_photos = Photo.all
     @list_of_photos = matching_photos.order({ :created_at => :desc })
     render({ :template => "photo_templates/index" }) 
+  end
+
+  def show
+    url_photo_id = params.fetch("path_id")
+
+    matching_photo_ids = Photo.where({ :id => url_photo_id })
+
+    @the_photo = matching_photo_ids.first
+
+    if @the_photo == nil
+      redirect_to("/404")
+    else
+      render({ :template => "photo_templates/show" })
+    end
   end
   
 end
