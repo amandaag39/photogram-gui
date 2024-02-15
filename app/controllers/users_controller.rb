939029fa115bef7372efaa0c049df4874b365a39
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-15 20:41:53 +0000

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,8 @@ class UsersController < ApplicationController
     render({ :template => "user_templates/index" })
   end
 
+  def show
+    render({ :template => "user_templates/show" })
+  end
+
 end
