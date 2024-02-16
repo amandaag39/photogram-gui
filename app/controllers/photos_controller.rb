9af2643797c5caf5b3477210489c960e1345ba41
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-16 19:16:52 +0000

    logic added for photos/index form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -33,4 +33,22 @@ class PhotosController < ApplicationController
     redirect_to("/photos")
   end
 
+  def create
+    # Parameters: {"input_image"=>"a", "input_caption"=>"b", "input_owner_id"=>"c"}
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+    input_owner_id = params.fetch("input_owner_id")
+    
+    a_new_photo = Photo.new
+
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    # render({ :template => "photo_templates/post"})
+    redirect_to("/photos/#{a_new_photo.id}")
+  end
+
 end
